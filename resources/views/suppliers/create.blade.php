e2b4f84f975000d8e519b99f603dcb2140d89d69
user <user@MacBook-Air-2.local>
2026-07-13 17:03:30 +0700

    menambahkan plugin untuk mempercantik form

--- resources/views/suppliers/create.blade.php
+++ resources/views/suppliers/create.blade.php
@@ -48,7 +48,7 @@
                         >
 
                         @error('name')
-                            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                            <p class="mt-8 text-sm text-red-600">{{ $message }}</p>
                         @enderror
                     </div>
 
